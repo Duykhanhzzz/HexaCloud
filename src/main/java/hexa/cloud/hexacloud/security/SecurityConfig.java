203f091ee015bd0b6c753fa6df8753cfa6da986a
duykhanhz <typeofbyte@gmail.com>
2025-06-12 01:20:46 +0700

    3

--- src/main/java/hexa/cloud/hexacloud/security/SecurityConfig.java
+++ src/main/java/hexa/cloud/hexacloud/security/SecurityConfig.java
@@ -12,8 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class SecurityConfig {
@@ -58,4 +57,5 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
+    
 }
