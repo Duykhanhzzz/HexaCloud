954373557890041e1e0bae09ccbce864e764e987
duykhanhz <typeofbyte@gmail.com>
2025-06-12 00:59:26 +0700

    1

--- src/main/java/hexa/cloud/hexacloud/repository/VpsAccessRepository.java
+++ src/main/java/hexa/cloud/hexacloud/repository/VpsAccessRepository.java
@@ -4,9 +4,7 @@ import hexa.cloud.hexacloud.model.VpsAccess;
 import hexa.cloud.hexacloud.model.VpsAccessId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface VpsAccessRepository extends JpaRepository<VpsAccess, VpsAccessId> {
-    List<VpsAccess> findByUserId(Long userId);
-    List<VpsAccess> findByVpsId(Integer vpsId);
+    
 }
