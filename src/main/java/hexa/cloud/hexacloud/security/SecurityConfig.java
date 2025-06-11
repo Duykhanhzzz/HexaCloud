6d906adc59714e83dbe237b5a7867bdb7bb10fba
duykhanhz <typeofbyte@gmail.com>
2025-06-11 16:00:51 +0700

    2

--- src/main/java/hexa/cloud/hexacloud/security/SecurityConfig.java
+++ src/main/java/hexa/cloud/hexacloud/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     @Bean
 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-        .cors(cors -> cors.configure(http))
+        .cors(cors -> {})
         .csrf(csrf -> csrf.disable())
         .formLogin(form -> form.disable())
         .httpBasic(basic -> basic.disable())
